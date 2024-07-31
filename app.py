a450e7d5a8e0406f6d683c206316a0a93e4b068e
Snehaperampalli <sneha.perampalli@gmail.com>
2024-07-31 09:20:48 +0530

    nutrition model

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import pandas as pd
 from flask_cors import CORS
 from datetime import datetime, timedelta
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.preprocessing import StandardScaler
 import joblib
 app = Flask(__name__)
@@ -552,7 +552,7 @@ X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
 # Train the model
-model = LinearRegression()
+model = DecisionTreeRegressor()
 model.fit(X_train_scaled, y_train)
 
 # Save the model and scaler
